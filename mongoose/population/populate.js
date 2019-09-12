7e3c1cda9970533176efab70369a8a1b7e324eee
pagella42 <pagella42@gmail.com>
2019-09-11 22:20:35 -0500

    fifth

--- mongoose/population/populate.js
+++ mongoose/population/populate.js
@@ -4,21 +4,19 @@ const Schema = mongoose.Schema
 const bookSchema = new Schema({ 
     title: String,
     author: String,
-    reviews: [reviewSchema]
+    reviews: [{type: Schema.Types.ObjectId, ref: 'Review'}] //reference to an array of Review documents
   })
   
   const reviewSchema = new Schema({ 
-    book: bookSchema,
+    book: {type: Schema.Types.ObjectId, ref: 'Book'}, //reference to a Book document
     reviewText: String,
-    critic: criticSchema
+    critic: {type: Schema.Types.ObjectId, ref: 'Critic'} //reference to a Critic document
   })
   
   const criticSchema = new Schema({ 
     name: String,
-    reviews: [reviewSchema],
+    reviews: [{type: Schema.Types.ObjectId, ref: 'Review'}], //reference to an array of Review documents
   })
-
-
 
 
   const Book = mongoose.model("Book", bookSchema)
